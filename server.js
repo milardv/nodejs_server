2a4dc9ec787283d9ffa35ffb042aea0f5deb97cc
Adrien Fenech <Adrien Fenech>
2016-11-21 10:32:50 +0100

    Content added

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 /**
- * Created by Adrien on 21/11/2016.
+ * Created for DevOps course on 21/11/2016.
  */
+const express = require('express');
+var jade = require('jade');
+const app = express();
+const port = 8080;
+
+app.use(express.static(__dirname + '/public'));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+
+var nbVisit = 0;
+
+app.get('/', (req, res) => {
+    res.render('index',
+        { nbVisit : ++nbVisit }
+    );
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        return console.log('something bad happened', err)
+    }
+    console.log(`server is listening on ${port}`)
+});
