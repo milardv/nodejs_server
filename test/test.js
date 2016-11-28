a3dd60b26fec427d5652f0a97ea85454a6765378
Valentin MILARD <valentin.milard1@gmail.com>
2016-11-28 16:55:07 +0100

    requite added

--- test/test.js
+++ test/test.js
@@ -2,6 +2,8 @@
  * Created by jeezou on 28/11/16.
  */
 
+var assert = require('assert');
+var todo_utils = require('../todo_utils');
 
 describe('#Error : Todo is null', function () {
     it('todo_utils._getAll should throw an error : todo is null', function () {
